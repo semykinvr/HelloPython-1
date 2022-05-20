b4f6319116f2556347b3a43f1bc757916a933d4e
semykinvr <semykinvr@mail.ru>
2022-05-20 23:13:18 +0300

    улучшен код для таск12

--- task12.py
+++ task12.py
@@ -1,26 +1,36 @@
 # 12. Для натурального n создать словарь индекс-значение, состоящий из элементов последовательности (3n + 1). например Для n = 6: {1: 4, 2: 7, 3: 10, 4: 13, 5: 16, 6: 19}
 
-# функция ввода ЦЕЛОГО ЧИСЛА с клавиатуры:
-def Input_int():
-    while True:
-        try:
-            console_input = int(input("Введите целое число: "))
-            return int(console_input)
-        except ValueError:
-            print("Неверный ввод.")
+# 1 ВАРИАНТ (короткий код): 
+import os
+os.system("cls")
+slovar = {n: (3 * n + 1) for n in range(1, int(input('Задайте количество элементов последовательности: '))+1)}
+print('\nСловарь: ', slovar)
 
-n = abs(Input_int())
-slovar1 = {} # определяем словарь1
-slovar2 = {} # определяем словарь2
-for i in range(1,n+1):
-    slovar1[i] = 3*i+1          # i-ключ, (3*i+1)-значение к этому ключу
-    slovar2.update({i:3*i+1})   # i-ключ, (3*i+1)-значение к этому ключу
 
-slovar3 = {j: (3 * j + 1) for j in range(1, n+1)} # j-ключ, (3*j+1)-значение к этому ключу
 
-print (type(slovar1))
-print ('Словарь1', slovar1)
-print (type(slovar2))
-print ('Словарь2', slovar2)
-print (type(slovar3))
-print ('Словарь3', slovar3)
+
+
+# # 2 ВАРИАНТ (длинный код): 
+# # функция ввода ЦЕЛОГО ЧИСЛА с клавиатуры:
+# def Input_int():
+#     while True:
+#         try:
+#             console_input = int(input("Введите целое число: "))
+#             return int(console_input)
+#         except ValueError:
+#             print("Неверный ввод.")
+# n = abs(Input_int())
+# slovar1 = {} # определяем словарь1
+# slovar2 = {} # определяем словарь2
+# for i in range(1,n+1):
+#     slovar1[i] = 3*i+1          # i-ключ, (3*i+1)-значение к этому ключу
+#     slovar2.update({i:3*i+1})   # i-ключ, (3*i+1)-значение к этому ключу
+
+# slovar3 = {j: (3 * j + 1) for j in range(1, n+1)} # j-ключ, (3*j+1)-значение к этому ключу
+
+# print (type(slovar1))
+# print ('Словарь1', slovar1)
+# print (type(slovar2))
+# print ('Словарь2', slovar2)
+# print (type(slovar3))
+# print ('Словарь3', slovar3)
