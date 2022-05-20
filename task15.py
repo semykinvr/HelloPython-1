13cd1853d12ab45885633ce82c3de950f892ef50
semykinvr <semykinvr@mail.ru>
2022-05-21 01:06:47 +0300

    улучшен код для таск15

--- task15.py
+++ task15.py
@@ -1,14 +1,28 @@
 # 15. Написать программу получающую набор произведений чисел от 1 до N. Пример: пусть N = 4, тогда [ 1, 2, 6, 24 ]
 
-# функция генерирует рандомное ЦЕЛОЕ ЧИСЛО:
+import os
 import random
-def Get_random_int(): return random.randint(1, 9)
+os.system("cls")
 
-n = Get_random_int()
-print(f'Задано число: {n}')
+n = random.randint(6, 10)
+print(f'Задано число N: {n}')
 massiv = [1]
-
 for i in range(2, n+1):
-    massiv.append(i * massiv[i-2])
+    massiv.append(massiv[i-2]*i)
+print(massiv, '\n')
 
-print(massiv)
+
+
+
+# # функция генерирует рандомное ЦЕЛОЕ ЧИСЛО:
+# import random
+# def Get_random_int(): return random.randint(1, 9)
+
+# n = Get_random_int()
+# print(f'Задано число: {n}')
+# massiv = [1]
+
+# for i in range(2, n+1):
+#     massiv.append(i * massiv[i-2])
+
+# print(massiv)
