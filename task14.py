2a0ff92da97efed54f4b711bf101e9419d4f4392
semykinvr <semykinvr@mail.ru>
2022-05-21 00:17:22 +0300

    улучшен код для таск14

--- task14.py
+++ task14.py
@@ -1,57 +1,71 @@
 # 14. Подсчитать сумму цифр в вещественном числе
 
-# функция генерирует рандомное ВЕЩЕСТВЕННОЕ ЧИСЛО:
-import random
-def Get_random_float(): return random.uniform(1, 999)
+# 1 ВАРИАНТ (короткий код): 
+spisok = list(map(int, str(input('Введите число: ')).replace('.', ''))) 
+print('Сумма цифр данного числа равна:', sum(spisok), '\n')
 
 
-# 1 вариант:
-a = float(Get_random_float()) # задаем случайное вещественное число из диапазона
-f = a
-print('Задано число:', a,'\n')
+# # # ВАРИАНТы (длинный код):
+# # функция генерирует рандомное ВЕЩЕСТВЕННОЕ ЧИСЛО:
+# import random
+# def Get_random_float(): return random.uniform(1, 999)
+
+# # 1 вариант:
+# n = input('Введите число: ')
+# stroka = str(n).replace('.', '') # переводим в строку, убираем точку
+# print(stroka)
+# stroka2 = list(map(int, stroka)) # переводим в числовой тип, и делаем список
+# print(stroka2)
+# summa = sum(stroka2) # считаем сумму
+# print('Сумма цифр данного числа равна:', summa, '\n')
 
 
-a = str(a).replace('.', '') # переводим в строковый тип, убираем точку
-d = a
-e = int(a)
-print(type(a))
-print('убрали точку и сделали строку', a,'\n')
+# # 2 вариант:
+# a = float(Get_random_float()) # задаем случайное вещественное число из диапазона
+# f = a
+# print('Задано число:', a,'\n')
 
-b = map(int, a) # перевели строку в объект
-print(type(b))
-print('перевели строку в объект ', b,'\n')
+# a = str(a).replace('.', '') # переводим в строковый тип, убираем точку
+# d = a
+# e = int(a)
+# print(type(a))
+# print('убрали точку и сделали строку', a,'\n')
 
-с = list(b) # переводим объект в массив
-print(type(с))
-print('переводим объект в массив', с,'\n')
+# b = map(int, a) # перевели строку в объект
+# print(type(b))
+# print('перевели строку в объект ', b,'\n')
 
-summa = sum(с) # считаем сумму
-print('1: Сумма цифр данного числа равна: ', summa, '\n')
+# с = list(b) # переводим объект в массив
+# print(type(с))
+# print('переводим объект в массив', с,'\n')
+
+# summa = sum(с) # считаем сумму
+# print('1: Сумма цифр данного числа равна: ', summa, '\n')
 
 
-# 2 вариант:
-massiv = []
-for i in range(0, len(d)): massiv.append(int(d[i])) # Добавление элемента в конец списка
-print(massiv)
-print('2: Сумма цифр данного числа равна: ', sum(massiv), '\n')
+# # 3 вариант:
+# massiv3 = []
+# for i in range(0, len(d)): massiv3.append(int(d[i])) # Добавление элемента в конец списка
+# print(massiv3)
+# print('3: Сумма цифр данного числа равна: ', sum(massiv3), '\n')
 
-# 3 вариант:
-summa = 0
-while e > 0:
-    summa = summa + (e % 10)
-    e = e // 10
-print(f'3: Сумма цифр данного числа равна: {summa} \n')
+# # 4 вариант:
+# summa = 0
+# while e > 0:
+#     summa = summa + (e % 10)
+#     e = e // 10
+# print(f'4: Сумма цифр данного числа равна: {summa} \n')
 
-# 4 вариант:
-massiv4 = list(str(f).split('.'))
-summa = 0
-for i in massiv4:
-    for j in i:
-        summa = summa + int(j)
-print(f'4: Сумма цифр данного числа равна: {summa} \n')
+# # 5 вариант:
+# massiv5 = list(str(f).split('.'))
+# summa = 0
+# for i in massiv5:
+#     for j in i:
+#         summa = summa + int(j)
+# print(f'5: Сумма цифр данного числа равна: {summa} \n')
 
-# 5 вариант:
-summa = 0
-for i in d:
-    summa = summa + int(i)
-print(f'5: Сумма цифр данного числа равна: {summa} \n')
+# # 6 вариант:
+# summa = 0
+# for i in d:
+#     summa = summa + int(i)
+# print(f'6: Сумма цифр данного числа равна: {summa} \n')
