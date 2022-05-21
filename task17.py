2ecb4d396372f60a6f6913cb72b8172b403cc86e
semykinvr <semykinvr@mail.ru>
2022-05-21 20:21:50 +0300

    улучшен код для таск17

--- task17.py
+++ task17.py
@@ -6,9 +6,14 @@ import random
 os.system("cls")
 
 # ВАРИАНТ (короткий код): 
-n = random.randint(5, 7)
+n = random.randint(3, 7)
 spisok = [i for i in range(-n,n+1)]
-print('список из N элементов: ', spisok)
+print('список из элементов от -N до N:', spisok)
+
+with open('text17.txt', 'w') as positions:
+     for i in range(0, int(n/2)):
+         positions.write(f'{random.randint(0, len(spisok))}\n') 
+
 multiply=1
 with open('text17.txt', 'r') as positions:
     for element in positions:
