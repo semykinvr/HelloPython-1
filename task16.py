cc32ccfb937273fcc5ae399cdaf031ba910a9c48
semykinvr <semykinvr@mail.ru>
2022-05-21 17:29:09 +0300

    улучшен код для таск16

--- task16.py
+++ task16.py
@@ -1,15 +1,29 @@
 # 16. Задать список из n чисел последовательности (1+〖1/n)〗^n и вывести на экран их сумму
 
-# функция генерирует рандомное ЦЕЛОЕ ЧИСЛО:
+# ВАРИАНТ (короткий код): 
+import os
 import random
-def Get_random_int(): return random.randint(1, 9)
+os.system("cls")
 
-n = Get_random_int()
-print(f'Задано число: {n}')
+n = random.randint(6, 10)
+spisok = [1+(1/i)**i for i in range(1, n+1)]
+print('Задана последовательность: ', spisok)
+print('Сумма чисел = ', round(sum(spisok), 2), '\n')
 
-massiv = []
 
-for i in range(1, n+1):
-    massiv.append(1+(1/i)**i)
-print(massiv)
-print(f'Сумма чисел в последовательности: {round(sum(massiv), 2)}')
+
+
+# # ВАРИАНТ (длинный код):
+# # функция генерирует рандомное ЦЕЛОЕ ЧИСЛО:
+# import random
+# def Get_random_int(): return random.randint(1, 9)
+
+# n = Get_random_int()
+# print(f'Задано число: {n}')
+
+# massiv = []
+
+# for i in range(1, n+1):
+#     massiv.append(1+(1/i)**i)
+# print(massiv)
+# print(f'Сумма чисел в последовательности: {round(sum(massiv), 2)}')
