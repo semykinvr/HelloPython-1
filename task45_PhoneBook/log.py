c0b06f8d6b057bc6ba5b1968cf0379eb30193cc7
semykinvr <semykinvr@mail.ru>
2022-05-24 23:44:23 +0300

    сделан task45_PhoneBook

--- task45_PhoneBook/log.py
+++ task45_PhoneBook/log.py
@@ -3,9 +3,9 @@ from datetime import datetime, date
 
 def log_to_file(surname, name, phone, comment, variant):
     if variant == '1':
-        with open('vertical.csv', 'a') as file:
+        with open('vertical.csv', 'a', encoding='utf-8') as file:
             file.write(f'{date.today()}\n{surname}\n{name}\n{phone}\n{comment}\n\n')
     elif variant == '2':
-        with open('horizontal.csv', 'a') as file:
+        with open('horizontal.csv', 'a', encoding='utf-8') as file:
             file.write(f'{date.today()};{surname};{name};{phone};{comment}\n')
     
